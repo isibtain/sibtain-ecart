fc50159cee06a4fcc84f06b14bb20b38a87d4ca7
isibtain <isibtainraza@gmail.com>
2020-10-09 05:10:37 -0500

    added view

---
+++ src/main/java/com/sibtain/supercms/models/data/Page.java
@@ -0,0 +1,20 @@
+package com.sibtain.supercms.models.data;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pages")
+@Data
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String slug;
+    private String content;
+    private int sorting;
+
+}
