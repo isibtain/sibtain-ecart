cd9e8334b0017f7fb51b89ff30e473d0de16a24b
isibtain <isibtainraza@gmail.com>
2020-10-09 09:43:52 -0500

    fixed add

--- src/main/java/com/sibtain/supercms/controllers/AdminPagesController.java
+++ src/main/java/com/sibtain/supercms/controllers/AdminPagesController.java
@@ -2,38 +2,81 @@ package com.sibtain.supercms.controllers;
 
 import com.sibtain.supercms.models.PageRepository;
 import com.sibtain.supercms.models.data.Page;
-import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
 
-    private PageRepository pageRepository;
+    @Autowired
+    private PageRepository pageRepo;
 
-    public AdminPagesController(PageRepository pageRepository) {
-        this.pageRepository = pageRepository;
-    }
+    // public AdminPagesController(PageRepository pageRepo) {
+    //     this.pageRepo = pageRepo;
+    // }
+
 
     @GetMapping
-    public String index(Model model){
-
-        List<Page> pages = pageRepository.findAll();
+    public String check(Model model){
+        List<Page> pages = pageRepo.findAll();
         model.addAttribute("pages",pages);
         return "admin/pages/index";
-
     }
 
+
+
     @GetMapping("/add")
-    public String add(@ModelAttribute Page page){
-//        model.addAttribute("page", new Page());
+    public String add(@ModelAttribute Page page) {
+
+        // model.addAttribute("page", new Page());
 
         return "admin/pages/add";
     }
+
+    @PostMapping("/add")
+    public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "admin/pages/add";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepo.findBySlug(slug);
+
+        if ( slugExists != null ) {
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+
+        } else {
+            page.setSlug(slug);
+            page.setSorting(100);
+
+            pageRepo.save(page);
+        }
+
+        return "redirect:/admin/pages/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model){
+
+        Page page = pageRepo.getOne(id);
+        model.addAttribute("page",page);
+        return "admin/pages/edit";
+    }
+
+
 }
--- src/main/java/com/sibtain/supercms/models/PageRepository.java
+++ src/main/java/com/sibtain/supercms/models/PageRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PageRepository extends JpaRepository<Page,Integer> {
 
+    Page findBySlug(String slug);
+
 }
