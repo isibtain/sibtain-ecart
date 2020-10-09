0a370b52c252129b91a502ca316fe408feab28d3
isibtain <isibtainraza@gmail.com>
2020-10-09 05:40:44 -0500

    added view

--- src/main/java/com/sibtain/supercms/controllers/AdminPagesController.java
+++ src/main/java/com/sibtain/supercms/controllers/AdminPagesController.java
@@ -1,15 +1,29 @@
 package com.sibtain.supercms.controllers;
 
+import com.sibtain.supercms.models.PageRepository;
+import com.sibtain.supercms.models.data.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
 
+    private PageRepository pageRepository;
+
+    public AdminPagesController(PageRepository pageRepository) {
+        this.pageRepository = pageRepository;
+    }
+
     @GetMapping
-    public String index(){
+    public String index(Model model){
+
+        List<Page> pages = pageRepository.findAll();
+        model.addAttribute("pages",pages);
         return "admin/pages/index";
 
     }
---
+++ src/main/java/com/sibtain/supercms/models/PageRepository.java
@@ -0,0 +1,8 @@
+package com.sibtain.supercms.models;
+
+import com.sibtain.supercms.models.data.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PageRepository extends JpaRepository<Page,Integer> {
+
+}
