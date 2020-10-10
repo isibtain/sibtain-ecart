271d7068d530109cc9c75fed9f75bdfc584b300f
isibtain <isibtainraza@gmail.com>
2020-10-10 17:20:32 -0500

    added images

--- src/main/java/com/sibtain/supercms/WebConfig.java
+++ src/main/java/com/sibtain/supercms/WebConfig.java
@@ -16,6 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/media/**")
-                .addResourceLocations("/media/");
+                .addResourceLocations("file:/C:/Users/sibta/Desktop/BestCMS/src/main/resources/static/media/");
     }
 }
