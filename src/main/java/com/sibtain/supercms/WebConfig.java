26d9256e2301e74dc4b0abd3dfe8df381695a5bb
isibtain <isibtainraza@gmail.com>
2020-10-10 03:40:30 -0500

    updated path

--- src/main/java/com/sibtain/supercms/WebConfig.java
+++ src/main/java/com/sibtain/supercms/WebConfig.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.awt.*;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
@@ -16,6 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/media/**")
-                .addResourceLocations("/media/");
+                .addResourceLocations("classpath:/static/media/");
     }
 }
