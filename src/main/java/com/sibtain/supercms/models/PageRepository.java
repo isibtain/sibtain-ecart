61c681d199d9c1a1eccf05895ae78461fa3e5cfb
isibtain <isibtainraza@gmail.com>
2020-10-09 20:41:39 -0500

    added view

--- src/main/java/com/sibtain/supercms/models/PageRepository.java
+++ src/main/java/com/sibtain/supercms/models/PageRepository.java
@@ -2,9 +2,13 @@ package com.sibtain.supercms.models;
 
 import com.sibtain.supercms.models.data.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PageRepository extends JpaRepository<Page,Integer> {
 
     Page findBySlug(String slug);
+
+    @Query("SELECT p FROM Page p WHERE p.id != :id and p.slug = :slug")
+    Page findBySlug(int id, String slug);
 
 }
