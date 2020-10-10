c27a69d45f46866663a86fca09999f293c379c7e
isibtain <isibtainraza@gmail.com>
2020-10-10 17:13:11 -0500

    added products

--- src/main/java/com/sibtain/supercms/WebConfig.java
+++ src/main/java/com/sibtain/supercms/WebConfig.java
@@ -5,8 +5,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.awt.*;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
