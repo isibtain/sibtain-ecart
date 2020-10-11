630c7b68fb1e077b1f49e00924705b0b0f6a8f8e
isibtain <isibtainraza@gmail.com>
2020-10-11 00:04:04 -0500

    path updates

--- src/main/java/com/sibtain/supercms/WebConfig.java
+++ src/main/java/com/sibtain/supercms/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/media/**")
-                //.addResourceLocations("classpath:/static/media/");
-                .addResourceLocations("file:/C:/Users/sibta/Desktop/BestCMS/src/main/resources/static/media/");
+                .addResourceLocations("classpath:/static/media/");
+                //.addResourceLocations("file:/C:/Users/sibta/Desktop/BestCMS/src/main/resources/static/media/");
 
 
     }
